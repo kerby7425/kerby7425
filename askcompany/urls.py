908f9363ec21333e0f4ccbfdbd7908c686951c7a
Chinseok Lee <me@askcompany.kr>
2019-02-13 11:46:49 +0900

    blog/urls.py 생성 및 이관

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -1,41 +1,7 @@
-"""askcompany URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path, re_path
-from blog.views import index, hello_times
-from blog.views import articles_by_year
-
-from django.urls import register_converter
-from blog.converters import FourDigitYearConverter
-from blog.converters import SlugUnicodeConverter
-
-register_converter(FourDigitYearConverter, 'year')
-register_converter(SlugUnicodeConverter, 'slug_unicode')
-
+from django.urls import include, path
 
 urlpatterns = [
-    path('articles/<year:year>/', articles_by_year),
-
-    # re_path('^blog/1/$', post_detail),
-    # re_path('^blog/1/edit/$', post_edit),
-
-    path('', index),
-    # re_path(r'^$', index),
-
     path('admin/', admin.site.urls),
-    path('blog/hello_times/<int:times>/', hello_times),
-    # re_path(r'blog/hello_times/(?P<times>\d+)/$', hello_times),
+    path('blog/', include('blog.urls')),
 ]
