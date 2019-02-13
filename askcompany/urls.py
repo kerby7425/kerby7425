106c6d264809b6d3b3c579f8ccc01341086efc94
Chinseok Lee <me@askcompany.kr>
2019-02-13 10:57:06 +0900

    year 커스텀 컨버터 등록 및 샘플 뷰 추가

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -16,8 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from blog.views import index, hello_times
+from blog.views import articles_by_year
+
+from django.urls import register_converter
+from blog.converters import FourDigitYearConverter
+
+register_converter(FourDigitYearConverter, 'year')
+
 
 urlpatterns = [
+    path('articles/<year:year>/', articles_by_year),
+
     # re_path('^blog/1/$', post_detail),
     # re_path('^blog/1/edit/$', post_edit),
 
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 
+def articles_by_year(request, year):
+    return HttpResponse(f'''
+        {year}년도에 대한 목록
+    ''')
+
+
 def index(request):
     return render(request, 'blog/index.html')
 
