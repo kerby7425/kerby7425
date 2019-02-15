9818d4221792289ecbe02033f519907f7355db6b
Chinseok Lee <me@askcompany.kr>
2019-02-15 10:10:12 +0900

    /blog/ 주소로 이동하는 root뷰 구현

--- askcompany/urls.py
+++ askcompany/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 from django.urls import include, path
+
+
+def root(request):
+    # TODO: 차후에 URL Reverse 기능
+    return HttpResponseRedirect('/blog/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
     path('shop/', include('shop.urls')),
+    path('', root),
 ]
