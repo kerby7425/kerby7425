12e3df83ee7e2590ff34c6f39af41b3c073f4999
Kerby <kerby1456@gmail.com>
2019-02-17 15:23:56 +0900

    model study

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    title = models.CharField(max_length=100)  # CharField는 길이 제한
+    content = models.TextField()   # TextField는 길이 제한 없음
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    
