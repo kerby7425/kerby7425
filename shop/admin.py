a710bcdf1842dc6decc6635623bdf8ed2b25c76d
Kerby <kerby1456@gmail.com>
2019-02-18 21:35:34 +0900

    shop app 작성

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-from .models import Item
-# Register your models here.
+from .models import Shop, Item
+
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'desc', 'address']
+    list_play_links = ['name']
+    search_fields = ['name']
+
+    def desc(self, shop):
+        return shop.content[:20] + "..."
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    pass
+
--- shop/models.py
+++ shop/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.conf import settings
+
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    desc = models.TextField()
+    address = models.CharField(max_length=50)
 
 class Item(models.Model):
+    shop = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     desc = models.TextField(blank=True)
     price = models.PositiveIntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    is_public = models.BooleanField(default=False)
+    # photo
