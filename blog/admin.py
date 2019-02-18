36f7ec9ce87195ddc1b45ddf2d61865ef1d053f3
Kerby <kerby1456@gmail.com>
2019-02-18 20:54:55 +0900

    blog app 작성

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,23 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
-admin.site.register(Post)
+# admin.site.register(Post)
+# admin.site.register(Comment)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'short_content', 'is_public', 'tags', 'created_at']
+    list_display_links = ['title']
+    list_filter = ['is_public']
+    search_fields = ['title']
+
+    def short_content(self, post):
+        return post.content[:20] + "..."
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    # list_diplay = ['id', 'post', 'message', 'created_at']
+
+    # def message(self, comment):
+    #     return comment.message[:20] + "..."
+    pass
--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,22 @@
 from django.db import models
-
+from django.conf import settings
 
 class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)  # CharField는 길이 제한
     content = models.TextField()   # TextField는 길이 제한 없음
+    is_public = models.BooleanField(default=False)
+    tags = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
